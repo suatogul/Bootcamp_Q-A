be7e4407779f5f7d6005091ce83a77a5a85ce159
suatogul <suatogul@gmail.com>
2022-12-10 23:53:41 +0100

    questions were edited

--- JS-2/MainQuestion/app.js
+++ JS-2/MainQuestion/app.js
@@ -3,14 +3,8 @@
  * Todo: *Add an email account to the list, inside the student object (email: 'jirik.evangeline@hicoders.ch',) by using firstname, lastname of the students and with @hicoders.ch domain name.
  * Todo: **Find the profession(s) of the richest parents.
  * Todo: *Create a login information for each student and add it as an object (logInfo:{username:fLastname,password:the day of the birthdate+the last_three_chars_of_the_lastname+zipCode}) Output:logInfo: { username: 'ejirik', password: '18rik1978' }
- * Todo: **Create a new list that shows only classes, students id, fullname, gender and average of the lessons for each student.
- * Todo: **Create a new list that shows only classnames and lessons's average.[{class:'8D',averages:{math:45,ict:63, chemistry: 54, biology: 46 }},{...}]
- * Todo: Create a new list that shows only classnames and all lessons's average.[{class:8A, average:75},{...}]
- * Todo: Show the GPA (not necessary)
- * Todo: *Find the most succesfull student(s) among the all classes.
  * Todo: Make a statistic [{fullClassName="8A",male:6,female:8,total:14},{...}]
  * Todo: Find the male, female and total number of students.{male:24,female:29,total:53}
- * Todo: Make a list of shirt sizes and amount of need.{s:6,m:18,l:12,xl:4,...}
  * Todo: Make a student list who was born in 2008.[{fullname:"John Doe",bod:24/11/2008},{...}] 
  * Todo: Compare the female and male students grade. Exmp Output: Female students are more successful
  * Todo: School secretary wants to send a warning letter some parents whose children grades' are so bad. You need to help the secretary. First task: find the five(5) children who has the lowest grade({id,firstname,lastname,average of the grades}). Second task: Make a new list with those five students parents name and their addresses.
@@ -21,13 +15,8 @@ import schoolData from './data.js'
 // console.log(fnc.listWithEmail.forEach(pClass => console.log(pClass.students)));
 // console.log(fnc.professionOfRichestParents());
 // console.log(fnc.listWithLogin[0].students);
-// console.log(fnc.averageOfLessonsEachStudent.forEach(pClass => console.log(pClass)))
-// console.log(fnc.classesAndLessonsAverages);
-// console.log(fnc.onlyClassesAndAverages)
-// console.log(fnc.mostSuccessfulStudent())
 // console.log(fnc.classMaleFemale);
 // console.log(fnc.allMaleFemale());
-// console.log(fnc.sizes())
 // console.log(fnc.bornIn2008)
 // console.log(fnc.whoIsBetter())
 // console.log(fnc.wishesOfTheSecretary())
